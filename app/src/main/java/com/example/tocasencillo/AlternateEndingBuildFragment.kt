e1f22a71c86482ea645f6f04b5c6eb05f70b035b
Cesc <francesc.cece@gmail.com>
2022-05-01 20:33:18 +0200

    Code cleanup on Fragments

--- app/src/main/java/com/example/tocasencillo/AlternateEndingBuildFragment.kt
+++ app/src/main/java/com/example/tocasencillo/AlternateEndingBuildFragment.kt
@@ -10,31 +10,10 @@ import com.example.tocasencillo.AssemblyActivity.Companion.positionInSong
 import com.example.tocasencillo.MySQLiteHelper.Companion.ALTERNATE_ENDING_TABLE
 import com.example.tocasencillo.databinding.FragmentAlternateEndingBuildBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AlternateEndingBuildFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AlternateEndingBuildFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private val myPosic: Int = positionInSong
     private lateinit var data: ArrayList<String>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     private var _binding: FragmentAlternateEndingBuildBinding? = null
 
@@ -81,25 +60,5 @@ class AlternateEndingBuildFragment : Fragment() {
         binding.tvLine2.text = data[0]
         binding.tvBar1.text = data[1]
         binding.tvBar2.text = data[2]
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AlternateEndingBuildFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AlternateEndingBuildFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
--- app/src/main/java/com/example/tocasencillo/ContentBuildFragment.kt
+++ app/src/main/java/com/example/tocasencillo/ContentBuildFragment.kt
@@ -10,31 +10,10 @@ import com.example.tocasencillo.AssemblyActivity.Companion.positionInSong
 import com.example.tocasencillo.MySQLiteHelper.Companion.CONTENT_TABLE
 import com.example.tocasencillo.databinding.FragmentContentBuildBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ContentBuildFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ContentBuildFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private val myPosic: Int = positionInSong
     private lateinit var data: ArrayList<String>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     private var _binding: FragmentContentBuildBinding? = null
     // This property is only valid between onCreateView and
@@ -83,25 +62,5 @@ class ContentBuildFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContentBuild.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ContentBuildFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
