3dbacc0de83406dd23e405597cb2ddb718fcb1d2
Cesc <francesc.cece@gmail.com>
2022-05-18 18:48:21 +0200

    Refactor code and delete GuideActivity.kt

--- app/src/main/java/com/example/tocasencillo/HomeActivity.kt
+++ app/src/main/java/com/example/tocasencillo/HomeActivity.kt
@@ -190,10 +190,6 @@ class HomeActivity : AppCompatActivity() {
                 val intent = Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
             }
-            item.toString() == getString(R.string.help) -> {
-                val intent = Intent(this, GuideActivity::class.java)
-                startActivity(intent)
-            }
             item.toString() == getString(R.string.sign_out) -> {
                 onBackPressed()
             }
